3908534d7ec231429746073f959341a3c5a11af8
João Vítor <joaovsoutosantos@gmail.com>
2021-11-10 20:12:18 -0300

    feat: create bubble component

---
+++ src/components/Bubble.tsx
@@ -0,0 +1,16 @@
+import { Box, BoxProps } from '@chakra-ui/react';
+
+type Variants = 'blue' | 'green';
+
+type BubbleProps = {
+  variant?: Variants;
+} & BoxProps;
+
+const gradientByVariant = {
+  green: 'linear(to-b, green.275 0%, green.250 48.96%, cyan.600 100%)',
+  blue: 'linear(to-b, cyan.400 0%, cyan.800 100%)',
+};
+
+export function Bubble({ variant = 'blue', ...rest }: BubbleProps) {
+  return <Box bgGradient={gradientByVariant[variant]} {...rest} />;
+}
