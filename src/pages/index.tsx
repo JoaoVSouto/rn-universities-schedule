6ac254a2cbbb3a4713876d31cb91163512398465
João Vítor <joaovsoutosantos@gmail.com>
2021-11-10 10:49:27 -0300

    feat: add heading and page title

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,27 @@
 import type { NextPage } from 'next';
+import Head from 'next/head';
+import { Container, Heading } from '@chakra-ui/react';
 
 const Home: NextPage = () => {
-  return <h1>hello world</h1>;
+  return (
+    <>
+      <Head>
+        <title>Horários Universitários</title>
+      </Head>
+
+      <Container maxW="container.xl" mt="24">
+        <Heading
+          as="h1"
+          size="4xl"
+          textAlign="center"
+          bgGradient="linear(114.85deg, blue.500 -16.23%, green.200 102.97%)"
+          bgClip="text"
+        >
+          Horários Universitários
+        </Heading>
+      </Container>
+    </>
+  );
 };
 
 export default Home;
