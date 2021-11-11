deca5425d1d0698fa0cd580fd74fd60e183807cc
João Vítor <joaovsoutosantos@gmail.com>
2021-11-11 09:29:33 -0300

    style: change default font-weight to 300

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -12,7 +12,7 @@ export default class MyDocument extends Document {
             crossOrigin="anonymous"
           />
           <link
-            href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;600;700&display=swap"
+            href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;600;700&display=swap"
             rel="stylesheet"
           />
 
