0ffcbe8bbc0efc003a25779dd0a4f5dc80c5aef1
João Vítor Souto <joaovsoutosantos@gmail.com>
2022-10-29 23:35:55 -0300

    feat: add pwa support

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -8,7 +8,10 @@ function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
       <Head>
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta
+          name="viewport"
+          content="minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no, user-scalable=no, viewport-fit=cover"
+        />
       </Head>
 
       <ChakraProvider theme={theme}>
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -18,6 +18,44 @@ export default class MyDocument extends Document {
 
           <meta name="theme-color" content="#1b1d20" />
 
+          <meta
+            name="application-name"
+            content="Horários de Aula - UFRN & IFRN"
+          />
+
+          <meta name="apple-mobile-web-app-capable" content="yes" />
+          <meta
+            name="apple-mobile-web-app-status-bar-style"
+            content="default"
+          />
+          <meta
+            name="apple-mobile-web-app-title"
+            content="Horários de Aula - UFRN & IFRN"
+          />
+
+          <meta name="format-detection" content="telephone=no" />
+          <meta name="mobile-web-app-capable" content="yes" />
+          <meta name="msapplication-TileColor" content="#1b1d20" />
+          <meta name="msapplication-tap-highlight" content="no" />
+
+          <link rel="apple-touch-icon" href="/icons/apple-touch-icon.png" />
+
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="32x32"
+            href="/icons/favicon-32x32.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="16x16"
+            href="/icons/favicon-16x16.png"
+          />
+
+          <link rel="manifest" href="/manifest.json" />
+          <link rel="shortcut icon" href="/icons/favicon.ico" />
+
           <meta property="og:type" content="website" />
           <meta
             property="og:url"
@@ -25,17 +63,14 @@ export default class MyDocument extends Document {
           />
           <meta
             property="og:site_name"
-            content="Horários Universitários - UFRN & IFRN"
+            content="Horários de Aula - UFRN & IFRN"
           />
           <meta
             property="og:keywords"
-            content="horários universitários, faculdade, ifrn, ufrn, horários, universidade, faculdade, horário de aula, aula, horário"
+            content="horários de aula, faculdade, ifrn, ufrn, horários, universidade, faculdade, horário de aula, aula, horário"
           />
           <meta property="og:locale" content="pt-BR" />
-          <meta
-            property="og:title"
-            content="Horários Universitários - UFRN & IFRN"
-          />
+          <meta property="og:title" content="Horários de Aula - UFRN & IFRN" />
           <meta
             property="og:description"
             content="Encontre facilmente os horários de aulas atualizados da UFRN e do IFRN!"
@@ -50,9 +85,18 @@ export default class MyDocument extends Document {
           <meta property="twitter:site:id" content="@perpetua_tech" />
           <meta property="twitter:creator" content="@perpetua_tech" />
           <meta property="twitter:creator:id" content="@perpetua_tech" />
+          <meta
+            property="twitter:url"
+            content="https://horarios-de-aula.vercel.app/"
+          />
+          <meta name="twitter:title" content="Horários de Aula - UFRN & IFRN" />
+          <meta
+            name="twitter:description"
+            content="Encontre facilmente os horários de aulas atualizados da UFRN e do IFRN!"
+          />
 
-          <meta property="og:image" content="TODO" />
-          <meta property="twitter:image" content="TODO" />
+          {/* <meta property="og:image" content="TODO" />
+          <meta property="twitter:image" content="TODO" /> */}
         </Head>
 
         <body>
