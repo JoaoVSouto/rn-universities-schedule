f5a066ca1a37852dcd3d26dcce388b274290eb2a
João Vítor <joaovsoutosantos@gmail.com>
2026-01-30 14:15:32 +0000

    fix: hydration issues

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,4 @@
+import { useSyncExternalStore } from 'react';
 import type { NextPage } from 'next';
 import Head from 'next/head';
 import { Box, ButtonGroup, Container, Flex, Heading } from '@chakra-ui/react';
@@ -15,7 +16,15 @@ import { Schedule } from 'components/Schedule';
 import { Signature } from 'components/Signature';
 import { FadeInUp } from 'components/FadeInUp';
 
+const noop = () => () => {};
+
 const Home: NextPage = () => {
+  const value = useSyncExternalStore(
+    noop,
+    () => 'client' as const,
+    () => 'server' as const
+  );
+
   const [university, setUniversity] = usePersistedState<Universities>(
     '@university-schedule',
     'ifrn'
@@ -31,112 +40,116 @@ const Home: NextPage = () => {
 
   return (
     <>
-      <Head>
-        <title>
-          Horários de Aula -
-          {typeof window !== 'undefined'
-            ? ` ${university.toUpperCase()}`
-            : ' UFRN & IFRN'}
-        </title>
-      </Head>
+      {value === 'client' && (
+        <>
+          <Head>
+            <title>
+              Horários de Aula -
+              {typeof window !== 'undefined'
+                ? ` ${university.toUpperCase()}`
+                : ' UFRN & IFRN'}
+            </title>
+          </Head>
 
-      <Container maxW="container.xl" mt="10" mb="8">
-        <FadeInUp>
-          <motion.div layout>
-            <Heading
-              as="h1"
-              fontSize={['4xl', '5xl', '6xl', '7xl']}
-              textAlign="center"
-              bgGradient="linear(114.85deg, blue.500 -16.23%, green.200 102.97%)"
-              bgClip="text"
-              userSelect="none"
-            >
-              Horários de Aula
-            </Heading>
-          </motion.div>
-        </FadeInUp>
-        <FadeInUp delay={0.4}>
-          <motion.div layout>
-            <Flex justify="center" mt="8">
-              <ButtonGroup
-                isAttached
-                color="gray.200"
-                fontSize={{ base: 'xl', lg: '2xl' }}
-                height={{ base: '12', lg: '16' }}
-                fontWeight="bold"
-              >
-                <UniversityButton
-                  university="ifrn"
-                  isActive={university === 'ifrn'}
-                  onClick={handleChangeUniversity('ifrn')}
-                />
-                <UniversityButton
-                  university="ufrn"
-                  isActive={university === 'ufrn'}
-                  onClick={handleChangeUniversity('ufrn')}
-                />
-              </ButtonGroup>
-            </Flex>
-          </motion.div>
-        </FadeInUp>
+          <Container maxW="container.xl" mt="10" mb="8">
+            <FadeInUp>
+              <motion.div layout>
+                <Heading
+                  as="h1"
+                  fontSize={['4xl', '5xl', '6xl', '7xl']}
+                  textAlign="center"
+                  bgGradient="linear(114.85deg, blue.500 -16.23%, green.200 102.97%)"
+                  bgClip="text"
+                  userSelect="none"
+                >
+                  Horários de Aula
+                </Heading>
+              </motion.div>
+            </FadeInUp>
+            <FadeInUp delay={0.4}>
+              <motion.div layout>
+                <Flex justify="center" mt="8">
+                  <ButtonGroup
+                    isAttached
+                    color="gray.200"
+                    fontSize={{ base: 'xl', lg: '2xl' }}
+                    height={{ base: '12', lg: '16' }}
+                    fontWeight="bold"
+                  >
+                    <UniversityButton
+                      university="ifrn"
+                      isActive={university === 'ifrn'}
+                      onClick={handleChangeUniversity('ifrn')}
+                    />
+                    <UniversityButton
+                      university="ufrn"
+                      isActive={university === 'ufrn'}
+                      onClick={handleChangeUniversity('ufrn')}
+                    />
+                  </ButtonGroup>
+                </Flex>
+              </motion.div>
+            </FadeInUp>
 
-        <Box pos="relative" maxW="985" mx="auto" mt="10">
-          <FadeInUp delay={0.6}>
-            <motion.div layout>
-              <Schedule data={chosenSchedule} />
+            <Box pos="relative" maxW="985" mx="auto" mt="10">
+              <FadeInUp delay={0.6}>
+                <motion.div layout>
+                  <Schedule data={chosenSchedule} />
 
-              <Bubble
-                w="32"
-                h="32"
-                transform="rotate(65deg)"
-                top="0.75rem"
-                left="-3.375rem"
-                delay={1}
-              />
-              <Bubble
-                w="14"
-                h="14"
-                transform="rotate(-95deg)"
-                top="2.3125rem"
-                right="1.5rem"
-                delay={1.4}
-              />
-              <Bubble
-                variant="green"
-                w="28"
-                h="28"
-                transform="rotate(-63deg)"
-                bottom="3.3125rem"
-                right="-3.5rem"
-                delay={1.1}
-              />
-              <Bubble
-                w="8"
-                h="8"
-                transform="rotate(-63deg)"
-                bottom="2.375rem"
-                right="14.6875rem"
-                delay={1.2}
-              />
-              <Bubble
-                variant="green"
-                w="12"
-                h="12"
-                transform="rotate(-63deg)"
-                bottom="0.5625rem"
-                left="0.125rem"
-                delay={1.6}
-              />
-            </motion.div>
-          </FadeInUp>
-        </Box>
+                  <Bubble
+                    w="32"
+                    h="32"
+                    transform="rotate(65deg)"
+                    top="0.75rem"
+                    left="-3.375rem"
+                    delay={1}
+                  />
+                  <Bubble
+                    w="14"
+                    h="14"
+                    transform="rotate(-95deg)"
+                    top="2.3125rem"
+                    right="1.5rem"
+                    delay={1.4}
+                  />
+                  <Bubble
+                    variant="green"
+                    w="28"
+                    h="28"
+                    transform="rotate(-63deg)"
+                    bottom="3.3125rem"
+                    right="-3.5rem"
+                    delay={1.1}
+                  />
+                  <Bubble
+                    w="8"
+                    h="8"
+                    transform="rotate(-63deg)"
+                    bottom="2.375rem"
+                    right="14.6875rem"
+                    delay={1.2}
+                  />
+                  <Bubble
+                    variant="green"
+                    w="12"
+                    h="12"
+                    transform="rotate(-63deg)"
+                    bottom="0.5625rem"
+                    left="0.125rem"
+                    delay={1.6}
+                  />
+                </motion.div>
+              </FadeInUp>
+            </Box>
 
-        <FadeInUp delay={0.8}>
-          <motion.div layout>
-            <Signature />
-          </motion.div>
-        </FadeInUp>
-      </Container>
+            <FadeInUp delay={0.8}>
+              <motion.div layout>
+                <Signature />
+              </motion.div>
+            </FadeInUp>
+          </Container>
+        </>
+      )}
 
       <Bubble
         w="80"
